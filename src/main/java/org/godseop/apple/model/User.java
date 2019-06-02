4b720711d8c8276714ae8352dcfdb309bc0613c7
welcometomycode <welcome2mycode@gmail.com>
2019-06-02 18:29:47 +0900

    형님 jpa 코드 넣었는데 형님이 만든 User 썼습니다 검토좀요

--- src/main/java/org/godseop/apple/model/User.java
+++ src/main/java/org/godseop/apple/model/User.java
@@ -1,12 +1,18 @@
 package org.godseop.apple.model;
 
 import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import org.apache.ibatis.type.Alias;
 
 @Data
+@Entity
 @Alias("user")
 public class User {
 
+	@Id
     private Integer seq;
     private String id;
     private String name;
