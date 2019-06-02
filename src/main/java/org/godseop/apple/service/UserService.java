4057421f7aa476cd500c78c2a956d498300f9238
welcometomycode <welcome2mycode@gmail.com>
2019-06-02 18:29:47 +0900

    형님 jpa 코드 넣었는데 형님이 만든 User 썼습니다 검토좀요

--- src/main/java/org/godseop/apple/service/UserService.java
+++ src/main/java/org/godseop/apple/service/UserService.java
@@ -2,6 +2,7 @@ package org.godseop.apple.service;
 
 import org.godseop.apple.model.User;
 import org.godseop.apple.repository.UserDao;
+import org.godseop.apple.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +15,16 @@ public class UserService {
 
     @Autowired
     private UserDao userDao;
+    
+    @Autowired
+    private UserRepository userRepository;
 
     public List<User> getUserListAll() {
         return userDao.selectUserListAll();
+    }
+    
+    public List<User> getUserListAllJpa() {
+        return userRepository.findAll();
     }
 
 }
