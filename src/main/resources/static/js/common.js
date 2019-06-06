30df8b705a07120c001494edabacfa824a242821
godseop <godseop@gmail.com>
2019-06-06 18:43:40 +0900

    로딩바 동기처리(async/await) / serializeObject 패턴매칭 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,42 +1,42 @@
+// input name=a[0].b[0].c 패턴 매칭을 위한 확장
+$.extend(FormSerializer.patterns, {
+    validate: /^[a-z][a-z0-9_]*((?:[\d+])*(?:.[^0-9][\w]*)?)*$/i,
+});
+
 function ajaxJson(url, object, callback, isLoadingBar=true) {
     $.post({
-        url: url,
+        url:         url,
         contentType: "application/json; charset=UTF-8",
-        data: JSON.stringify(object),
-        beforeSend: isLoadingBar && showLoadingBar,
-        success: callback,
+        data:        JSON.stringify(object),
+        beforeSend:  isLoadingBar && showLoadingBar,
+        success:     callback,
         error: function() {
             alert("서버로 요청중 에러가 발생했습니다.");
         },
-        complete: isLoadingBar && hideLoadingBar,
+        complete:    isLoadingBar && hideLoadingBar,
     });
 }
 
 function ajaxEncoded(url, object, callback, isLoadingBar=true) {
     $.post({
-        url: url,
+        url:         url,
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-        data: object,
-        beforeSend: isLoadingBar && showLoadingBar,
-        success: callback,
+        data:        object,
+        beforeSend:  isLoadingBar && showLoadingBar,
+        success:     callback,
         error: function() {
             alert("서버로 요청중 에러가 발생했습니다.");
         },
-        complete: isLoadingBar && hideLoadingBar,
+        complete:    isLoadingBar && hideLoadingBar,
     });
 }
 
 // 로딩바 보임
-function showLoadingBar() {
-    $(".sk-wave").show();
+async function showLoadingBar() {
+    await $(".sk-wave").show();
 }
 
 // 로딩바 숨김
-function hideLoadingBar() {
-    $(".sk-wave").hide();
-}
-
-
-function sleep(millisecond) {
-    return new Promise(resolve => setTimeout(resolve, millisecond));
+async function hideLoadingBar() {
+    await $(".sk-wave").hide();
 }
