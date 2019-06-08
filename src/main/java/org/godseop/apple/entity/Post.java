a3c3390a774cbf7deb8ab1a8afa2e63319284cbb
godseop <godseop@gmail.com>
2019-06-08 12:34:59 +0900

    entity 정리 및 user -> member 변경

--- src/main/java/org/godseop/apple/entity/Post.java
+++ src/main/java/org/godseop/apple/entity/Post.java
@@ -1,35 +1,44 @@
 package org.godseop.apple.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
+import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
-@Entity
+@Entity(name = "T_POST")
+@EqualsAndHashCode(of = "id")
 public class Post {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "uid")
+    private Member member;
+
+    @Column(nullable = false)
     private String title;
-    
+
     private String content;
-    
+
     @CreationTimestamp
-    private LocalDateTime createdOn;
-    
-    @OneToMany(mappedBy="post")
-    private List<Comment> comments;
-    
+    private LocalDateTime regDate;
+
+    @UpdateTimestamp
+    private LocalDateTime modDate;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> commentList;
+
+    @OneToMany(mappedBy = "post")
+    private List<HashTag> hashTagList;
+
 }
--- src/main/java/org/godseop/apple/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.godseop.apple.repository;
-
-import org.godseop.apple.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-	
-
-}
