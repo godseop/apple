ce92a8a8c758fbc512d4ad84143794177b7f9f69
godseop <godseop@gmail.com>
2019-07-23 02:39:57 +0900

    login logout handler

--- src/main/java/org/godseop/apple/security/AppleLoginSuccessHandler.java
+++ src/main/java/org/godseop/apple/security/AppleLoginSuccessHandler.java
@@ -3,21 +3,49 @@ package org.godseop.apple.security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
+@Component
 public class AppleLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        SavedRequest savedRequest
+                = requestCache.getRequest(request, response);
 
+        if (savedRequest == null) {
+            clearAuthenticationAttributes(request);
+            return;
+        }
+        String targetUrlParam = getTargetUrlParameter();
+        if (isAlwaysUseDefaultTargetUrl()
+                || (targetUrlParam != null
+                && StringUtils.hasText(request.getParameter(targetUrlParam)))) {
+            requestCache.removeRequest(request, response);
+            clearAuthenticationAttributes(request);
+            return;
+        }
+
+        clearAuthenticationAttributes(request);
     }
 
     @Override
     protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
 
+    }
+
+    public void setRequestCache(RequestCache requestCache) {
+        this.requestCache = requestCache;
     }
 
 
